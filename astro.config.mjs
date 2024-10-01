01d1b5f4f20872c55b7ee1417e32f4e80a8b8c43
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-01 14:47:48 +0200

    chore(build): output for vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 import unocss from "unocss/astro";
 
 import icons from 'unplugin-icons/vite'
 import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+
   integrations: [
     unocss({
       injectReset: true
