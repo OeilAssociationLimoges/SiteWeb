02ef091270add709e463a12804c2794c90efd692
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-01 14:45:38 +0200

    feat: add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,24 @@
 import { defineConfig } from 'astro/config';
 import unocss from "unocss/astro";
 
+import icons from 'unplugin-icons/vite'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+
 export default defineConfig({
-  integrations: [unocss({ injectReset: true })]
+  integrations: [
+    unocss({
+      injectReset: true
+    })
+  ],
+
+  vite: {
+    plugins: [
+      icons({
+        compiler: "astro",
+        customCollections: {
+          "local": FileSystemIconLoader("./assets/icons")
+        }
+      })
+    ]
+  }
 });
