8aecc7d19eec4ad25e10e19f887b2df4d101cf53
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-01 16:25:45 +0200

    feat: add agenda.yml

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface AgendaItem {
+  date: string
+  type: "event"
+  location: string
+  "location-link"?: string
+  description: string
+}
+
+declare module "*.yml" {
+  const value: Array<AgendaItem>;
+  export default value;
+}
