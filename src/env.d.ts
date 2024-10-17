30f21d633e31f1a4a4a3d953c53dfd2c72246add
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-17 22:23:39 +0200

    chore: add type for paypal base

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,13 +2,22 @@
 
 interface AgendaItem {
   date: string
-  type: "event"
   location: string
   "location-link"?: string
   description: string
 }
 
+interface ProductItem {
+  id: string
+  name: string
+  description: string
+  price: number
+  image: string
+}
+
 declare module "*.yml" {
-  const value: Array<AgendaItem>;
+  const value: Array<AgendaItem | ProductItem>;
   export default value;
 }
+
+declare const __PAYPAL_BASE__: string;
