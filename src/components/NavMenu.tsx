755a16e25bbb7a3f2c59b2a12d36c27c6fcf6467
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-17 21:37:51 +0200

    refactor: move parts into components for easier usage

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,6 +1,5 @@
 import { createSignal, type Component, Show } from "solid-js";
 import IconOeil from '~icons/local/oeil'
-import IconOpenInNew from '~icons/mdi/open-in-new'
 import IconClose from '~icons/mdi/close'
 import IconMenu from '~icons/mdi/menu'
 import UserProfile from "./UserProfile";
@@ -14,26 +13,26 @@ const NavMenu: Component = () => {
   return (
     <nav class="fixed top-0 inset-x-0 z-20 h-24 flex items-center justify-between px-8 bg-black md:backdrop-blur-xl md:bg-black/80">
       <div class="flex items-center justify-between w-full gap-6">
-        <a class="text-white">
+        <a class="text-white" href="/">
           <IconOeil class="text-6xl" />
         </a>
 
         <Show when={open() || !isContainedInMenu()}>
           <div class="md:bg-transparent h-full w-full md:gap-6 bg-black pb-6 md:pb-0 z-20 fixed md:relative top-24 md:top-0 md:z-0 left-0 right-0 gap-2 text-white flex flex-col md:flex-row items-center"
           >
-            <a class="py-6 block font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors" href="#agenda">
+            <a class="py-6 block font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors" href="/#agenda">
               Agenda
             </a>
-            <a class="py-6 block font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors" href="#presentation">
+            <a class="py-6 block font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors" href="/#presentation">
               Présentation
             </a>
-            <a class="py-6 block font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors" href="#partenaires">
+            <a class="py-6 block font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors" href="/#partenaires">
               Partenaires
             </a>
-            <a class="py-6 items-center w-full md:w-auto justify-center gap-2 flex font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors mr-auto" href="/">
+            <a class="py-6 block font-300 text-4 text-white/80 tracking-wide hover:text-white transition-colors mr-auto" href="/shop">
               Shop
-              <IconOpenInNew />
             </a>
+
             <UserProfile />
           </div>
         </Show>
