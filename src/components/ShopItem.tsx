17b91df5abe51155e31a52c4e487ef94f6085fa6
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-19 10:34:26 +0100

    feat: add new shop items and details page

---
+++ src/components/ShopItem.tsx
@@ -0,0 +1,71 @@
+import { createEffect, createMemo, createSignal, For, onMount, type Component } from "solid-js";
+import createEmblaCarousel from 'embla-carousel-solid';
+
+const ShopItem: Component<{ product: ProductItem }> = (props) => {
+  const [emblaRef, emblaApi] = createEmblaCarousel(
+    () => ({ loop: true })
+  );
+
+  const [index, setIndex] = createSignal(0);
+  const images = createMemo(() => props.product.variants.map((variant) => variant.images[0]));
+
+  createEffect(() => {
+    const api = emblaApi();
+    if (!api) return;
+
+    setIndex(api.selectedScrollSnap());
+    api.on('select', () => setIndex(api.selectedScrollSnap()));
+  })
+  
+  return (
+    <div class="relative shrink-0 flex flex-col w-full md:w-xl border-3 border-black shadow-[-8px_8px_0_#000]">
+      <div class="absolute z-10 pointer-events-none top-2 right-2 flex flex-col gap-1 items-end">
+        <span class="text-sm sm:text-base w-fit bg-black text-white px-3 py-.5">
+          {props.product.variants[index()].name}
+        </span>
+        <span class="text-xs sm:text-sm w-fit bg-black text-white px-3 py-.5">
+          {index() + 1} / {images().length}
+        </span>
+      </div>
+
+      <div class="overflow-hidden cursor-grab" ref={emblaRef}>
+        <div class="flex w-full h-300px">
+          <For each={images()}>
+            {(image) => (
+              <div class="flex-[0_0_100%] min-w-0">
+                <img
+                  src={`/_products/${image}`}
+                  alt={props.product.name}
+                  class="w-full h-full object-contain bg-white select-none"
+                />
+              </div>
+            )}
+          </For>
+        </div>
+      </div>
+      
+      <div class="flex items-center justify-between gap-2 border-t-3 border-black p-4 bg-black text-white font-mono">
+        <p>
+          &gt; {props.product.name}
+        </p>
+        <p class="text-lg shrink-0">
+          {props.product.price} €
+        </p>
+      </div>
+      <div class="p-4 space-y-6">
+        <p class="">
+          {props.product.description}
+        </p>
+
+        <a
+          class="block text-center bg-black text-white w-full px-2 py-4 font-mono hover:bg-black/75 transition-colors"
+          href={`/product/${props.product.id}`}  
+        >
+          voir le produit
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ShopItem;
---
+++ src/components/shop/details/ShopDetailsArticle.tsx
@@ -0,0 +1,97 @@
+import { createSignal, For, Match, Switch, type Component } from "solid-js";
+import createEmblaCarousel from 'embla-carousel-solid';
+
+const ShopDetailsArticle: Component<{ product: ProductItem }> = (props) => {
+  let variantSelectedRef = document.getElementById("user-variant-selected") as HTMLInputElement;
+  const [selectedVariantIndex, setSelectedVariantIndex] = createSignal(variantSelectedRef.value);
+  const selectedVariant = () => props.product.variants.find((variant) => variant.id === selectedVariantIndex());
+
+  const [emblaRef] = createEmblaCarousel(
+    () => ({ loop: true })
+  );
+
+  return (
+    <div class="flex w-full justify-between gap-6">
+      <div class="shrink-0 flex flex-col gap-2 border-black border-2 w-fit">
+        <div class="overflow-hidden cursor-grab w-400px" ref={emblaRef}>
+          <div class="flex">
+            <For each={selectedVariant()!.images}>
+              {(image) => (
+                <div class="flex-[0_0_100%] min-w-0">
+                  <img
+                    src={`/_products/${image}`}
+                    alt={props.product.name}
+                    class="w-400px h-400px object-contain bg-white select-none"
+                  />
+                </div>
+              )}
+            </For>
+          </div>
+        </div>
+
+        <hr class="border-black border-t-2" />
+
+        <div class="flex flex-wrap w-400px gap-2 p-4">
+          <For each={props.product.variants}>
+            {(variant) => (
+              <div class="flex items-center gap-2 w-fit py-1.5 px-3 cursor-pointer select-none"
+                classList={{
+                  "bg-black text-white": selectedVariantIndex() === variant.id
+                }}
+                onClick={() => {
+                  setSelectedVariantIndex(variant.id);
+                  variantSelectedRef.value = variant.id;
+                }}
+              >
+                <label for={variant.id}>{variant.name}</label>
+              </div>
+            )}
+          </For>
+        </div>
+      </div>
+      <div class="flex flex-col font-mono w-full">
+        <h1 class="text-xl">{props.product.name}</h1>
+        <p class="text-2xl font-600">{props.product.price} €</p>
+
+        <div class="flex flex-col gap-4 mt-8">
+          <For each={props.product.inputs}>
+            {(input) => (
+              <label class="flex flex-col">
+                <span class="mb-2">
+                  {input.name}
+                </span>
+
+                <Switch>
+                  <Match when={input.type === "text" && input}>
+                    {input => (
+                      <input
+                        type="text"
+                        placeholder={input().placeholder}
+                        class="w-full p-2 border-black border-2"
+                      />
+                    )}
+                  </Match>
+                  <Match when={input.type === "select" && input}>
+                    {input => (
+                          <select id={input().id} class="w-full p-2 border-black border-2 focus:outline-black focus:outline-offset-4">
+                      <For each={input().options}>
+                          {(option) => (
+                            <option value={option.value}>
+                              {option.name}
+                            </option>
+                          )}
+                        </For>
+                      </select>
+                    )}
+                  </Match>
+                </Switch>
+              </label>
+            )}
+          </For>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShopDetailsArticle;
