bfffa4bfe9380a48fb4a99b237022eb8585ebac7
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-19 17:39:56 +0100

    feat: create order API (1/2 paypal steps)

---
+++ src/pages/api/orders/index.ts
@@ -0,0 +1,27 @@
+import type { APIRoute } from 'astro';
+import { createOrder, json } from '../../../utils/api';
+import products from "../../../data/products.yml";
+
+/**
+ * `POST /api/orders`, permet la création d'une commande
+ * en utilisant l'API de PayPal.
+ */
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json() as {
+    id: string;
+  };
+
+  const product = (<ProductItem[]>products).find(product => product.id === body.id);
+  if (!product) {
+    return json({ error: "Le produit séléctionné est introuvable." }, 404);
+  }
+
+  try {
+    const { jsonResponse, httpStatusCode } = await createOrder(product.price);
+    return json(jsonResponse, httpStatusCode);
+  }
+  catch (error) {
+    console.error("[POST /api/orders]:", error);
+    return json({ error: "Une erreur s'est produite lors de la création de la commande." }, 500);
+  }
+};
