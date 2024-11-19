78c17bae1319e228e1779c5499f1f74c3b4d3abf
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-19 19:11:59 +0100

    fix(shop/details): centering on large desktops

--- src/components/shop/details/ShopDetailsArticle.tsx
+++ src/components/shop/details/ShopDetailsArticle.tsx
@@ -3,6 +3,7 @@ import createEmblaCarousel from 'embla-carousel-solid';
 import { loadScript } from "@paypal/paypal-js";
 import { createStore } from "solid-js/store";
 import store from "../../../utils/store";
+import MdiChevronLeft from '~icons/mdi/chevron-left'
 
 const ShopDetailsArticle: Component<{ product: ProductItem }> = (props) => {
   const [paypalButtonsContainer, setPaypalButtonsContainer] = createSignal<HTMLDivElement>();
@@ -140,102 +141,111 @@ const ShopDetailsArticle: Component<{ product: ProductItem }> = (props) => {
   }));
 
   return (
-    <div class="flex flex-col lg:flex-row h-full w-full gap-6">
-      <div class="relative h-full">
-        <div class="shrink-0 flex flex-col gap-4 border-black border-2 lg:w-fit h-fit sticky top-28">
-          <div class="overflow-hidden cursor-grab w-full lg:w-400px" ref={emblaRef}>
-            <div class="flex">
-              <For each={variant()!.images}>
-                {(image) => (
-                  <div class="flex-[0_0_100%] min-w-0">
-                    <img
-                      src={`/_products/${image}`}
-                      alt={props.product.name}
-                      class="w-full h-300px lg:w-400px lg:h-400px object-contain bg-white select-none"
-                    />
-                  </div>
+    <>
+      <div class="lg:w-[calc(400px+1.5rem+532px)] lg:mx-auto">
+        <a href="/shop" class="mb-8 flex items-center gap-2 w-fit hover:bg-black/5 focus:bg-black/8 px-4 py-2 -ml-4">
+          <MdiChevronLeft />
+          <p>Revenir à la page des produits</p>
+        </a>
+      </div>
+
+      <div class="flex flex-col lg:flex-row h-full w-full gap-6 justify-center">
+        <div class="relative h-full">
+          <div class="shrink-0 flex flex-col gap-4 border-black border-2 lg:w-fit h-fit sticky top-28">
+            <div class="overflow-hidden cursor-grab w-full lg:w-400px" ref={emblaRef}>
+              <div class="flex">
+                <For each={variant()!.images}>
+                  {(image) => (
+                    <div class="flex-[0_0_100%] min-w-0">
+                      <img
+                        src={`/_products/${image}`}
+                        alt={props.product.name}
+                        class="w-full h-300px lg:w-400px lg:h-400px object-contain bg-white select-none"
+                      />
+                    </div>
+                  )}
+                </For>
+              </div>
+            </div>
+
+            <select class="bg-white px-2 py-3.5 outline-none border-black border-t-2 hover:bg-black/2 focus:bg-black/5 transition-colors"
+              onChange={(event) => {
+                const value = event.currentTarget.value;
+                setVariantID(value);
+              }}
+            >
+              <For each={props.product.variants}>
+                {(variant) => (
+                  <option value={variant.id}
+                    selected={variant.id === variantID()}
+                  >
+                    {variant.name}
+                  </option>
                 )}
               </For>
-            </div>
+            </select>
           </div>
+        </div>
 
-          <select class="bg-white px-2 py-3.5 outline-none border-black border-t-2 hover:bg-black/2 focus:bg-black/5 transition-colors"
-            onChange={(event) => {
-              const value = event.currentTarget.value;
-              setVariantID(value);
-            }}
-          >
-            <For each={props.product.variants}>
-              {(variant) => (
-                <option value={variant.id}
-                  selected={variant.id === variantID()}
-                >
-                  {variant.name}
-                </option>
+        <div class="flex flex-col font-mono w-full lg:max-w-532px">
+          <h1 class="text-xl text-center mt-6 lg:text-start lg:mt-0">{props.product.name}</h1>
+          <p class="text-2xl font-600 text-center lg:text-start">{props.product.price} €</p>
+
+          <div class="flex flex-col gap-4 mt-8">
+            <For each={props.product.inputs}>
+              {(input) => (
+                <label class="flex flex-col">
+                  <span class="mb-2">
+                    {input.name}
+                  </span>
+
+                  <Switch>
+                    <Match when={input.type === "text" && input}>
+                      {input => (
+                        <input
+                          type="text"
+                          placeholder={input().placeholder}
+                          class="w-full p-2 border-black border-2 bg-white text-black outline-none hover:bg-black/2 focus:bg-black/5 transition-colors"
+                        />
+                      )}
+                    </Match>
+                    <Match when={input.type === "select" && input}>
+                      {input => (
+                        <select id={input().id} class="w-full p-2 border-black border-2 bg-white text-black outline-none hover:bg-black/2 focus:bg-black/5 transition-colors">
+                          <For each={input().options}>
+                            {(option) => (
+                              <option value={option.value}>
+                                {option.name}
+                              </option>
+                            )}
+                          </For>
+                        </select>
+                      )}
+                    </Match>
+                  </Switch>
+                </label>
               )}
             </For>
-          </select>
+
+            <Show when={store.user} fallback={
+              <div class="flex flex-col gap-2 mt-8">
+                <p class="font-sans text-center text-black/75 text-xs lg:text-base">
+                  Vous devez vous identifier pour effectuer un paiement.
+                </p>
+                <a
+                  href="/identification"
+                  class="font-mono bg-black text-white text-center py-3"
+                >
+                  S'identifier avec Biome
+                </a>
+              </div>
+            }>
+              <div ref={setPaypalButtonsContainer} class="w-full max-w-500px mx-auto mt-8 z-0" />
+            </Show>
+          </div>
         </div>
       </div>
-
-      <div class="flex flex-col font-mono w-full">
-        <h1 class="text-xl text-center mt-6 lg:text-start lg:mt-0">{props.product.name}</h1>
-        <p class="text-2xl font-600 text-center lg:text-start">{props.product.price} €</p>
-
-        <div class="flex flex-col gap-4 mt-8">
-          <For each={props.product.inputs}>
-            {(input) => (
-              <label class="flex flex-col">
-                <span class="mb-2">
-                  {input.name}
-                </span>
-
-                <Switch>
-                  <Match when={input.type === "text" && input}>
-                    {input => (
-                      <input
-                        type="text"
-                        placeholder={input().placeholder}
-                        class="w-full p-2 border-black border-2 bg-white text-black outline-none hover:bg-black/2 focus:bg-black/5 transition-colors"
-                      />
-                    )}
-                  </Match>
-                  <Match when={input.type === "select" && input}>
-                    {input => (
-                      <select id={input().id} class="w-full p-2 border-black border-2 bg-white text-black outline-none hover:bg-black/2 focus:bg-black/5 transition-colors">
-                        <For each={input().options}>
-                          {(option) => (
-                            <option value={option.value}>
-                              {option.name}
-                            </option>
-                          )}
-                        </For>
-                      </select>
-                    )}
-                  </Match>
-                </Switch>
-              </label>
-            )}
-          </For>
-
-          <Show when={store.user} fallback={
-            <div class="flex flex-col gap-2 mt-8">
-              <p class="font-sans text-center text-black/75 text-xs lg:text-base">
-                Vous devez vous identifier pour effectuer un paiement.
-              </p>
-              <a
-                href="/identification"
-                class="font-mono bg-black text-white text-center py-3"
-              >
-                S'identifier avec Biome
-              </a>
-            </div>
-          }>
-            <div ref={setPaypalButtonsContainer} class="w-full max-w-500px mx-auto mt-8 z-0" />
-          </Show>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
